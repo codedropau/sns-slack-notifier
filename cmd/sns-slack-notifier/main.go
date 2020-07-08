f29e0546d1be75b42c0bc23e0fa4f087b6b550fe
Kim Pepper <kim@previousnext.com.au>
2020-07-08 13:04:51 +1000

    Initial commit

---
+++ cmd/sns-slack-notifier/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/slack-go/slack"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	cliSlackToken        = kingpin.Flag("token", "Slack token").Envar("SLACK_TOKEN").String()
+	cliSlackChannel      = kingpin.Flag("channel-id", "Slack channel ID").Envar("SLACK_CHANNEL_ID").String()
+	cliSlackMessageColor = kingpin.Flag("color", "Slack mesage color").Envar("SLACK_MESSAGE_COLOR").Default(ColourGood).String()
+)
+
+const (
+	// ColourGood is the slack colour for good.
+	ColourGood = "good"
+
+	// ColourDanger is the slack colour for danger.
+	ColourDanger = "danger"
+
+	// ColourWarning is the slack colour for warning.
+	ColourWarning = "warning"
+)
+
+func main() {
+	kingpin.Parse()
+	lambda.Start(HandleRequest)
+}
+
+// HandleRequest contains the code which will be executed.
+func HandleRequest(ctx context.Context, snsEvent events.SNSEvent) error {
+	slackApi := slack.New(*cliSlackToken)
+	for _, record := range snsEvent.Records {
+		snsRecord := record.SNS
+		attachment := slack.Attachment{
+			Color: *cliSlackMessageColor,
+			Text: snsRecord.Message,
+			Footer: fmt.Sprintf(":skpr: %s Source: %s MessageID: %s Topic: %s", snsRecord.Timestamp.Format(time.UnixDate), record.EventSource, snsRecord.MessageID, snsRecord.TopicArn),
+		}
+		_, _, err := slackApi.PostMessage(*cliSlackChannel, slack.MsgOptionText(snsRecord.Subject, false), slack.MsgOptionAttachments(attachment))
+		if err != nil {
+			return err
+		}
+		fmt.Printf("[%s %s] Message = %s \n", record.EventSource, snsRecord.Timestamp, snsRecord.Message)
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/codedropau/sns-slack-notifier
+
+go 1.13
+
+require (
+	github.com/aws/aws-lambda-go v1.17.0
+	github.com/aws/aws-sdk-go v1.33.3 // indirect
+	github.com/codedropau/cloudfront-certificate-expiration v0.0.1 // indirect
+	github.com/slack-go/slack v0.6.5
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
